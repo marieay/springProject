65751adb2e370ce71ca312648f808a379cfca433
timcrelier <82259470+timcrelier@users.noreply.github.com>
2021-04-22 16:33:13 +0200

    Update Data_Cleaning.R

--- Data_Cleaning.R
+++ Data_Cleaning.R
@@ -43,110 +43,12 @@ ESS.subset <- subset(ESS.subset, select = - c(stfdem, gndr, agea, pdjobyr, hinct
 
 #### What is missing: Trust in Institutions Mean, Winner Loser Dummy Variable
 
-# Trust in institution Mean (Needs to be shortened with aggregate())
-trstinst_BE <- ESS.data[c(1202:3070),c(6,13:19)]
-summary(trstinst_BE)
-assign("mean_trstinst_BE", 5.028)
-
-trstinst_BG <- ESS.data[c(3071:5330),c(6,13:19)]
-summary(trstinst_BG)
-assign("mean_trstinst_BG", 2.867)
-
-trstinst_CH <- ESS.data[c(5331:6823),c(6,13:19)]
-summary(trstinst_CH)
-assign("mean_trstinst_CH", 5.734)
-
-trstinst_CY <- ESS.data[c(6824:7939),c(6,13:19)]
-summary(trstinst_CY)
-assign("mean_trstinst_CY", 3.770)
-
-trstinst_CZ <- ESS.data[c(7940:9948),c(6,13:19)]
-summary(trstinst_CZ)
-assign("mean_trstinst_CZ", 3.750)
-
-trstinst_DE <- ESS.data[c(9949:12906),c(6,13:19)]
-summary(trstinst_DE)
-assign("mean_trstinst_DE", 4.871)
-
-trstinst_DK <- ESS.data[c(12907:14556),c(6,13:19)]
-summary(trstinst_DK)
-assign("mean_trstinst_DK", 6.289)
-
-trstinst_EE <- ESS.data[c(14557:16936),c(6,13:19)]
-summary(trstinst_EE)
-assign("mean_trstinst_EE", 4.459)
-
-trstinst_ES <- ESS.data[c(16937:18825),c(6,13:19)]
-summary(trstinst_ES)
-assign("mean_trstinst_ES", 3.630)
-
-trstinst_FI <- ESS.data[c(18826:21022),c(6,13:19)]
-summary(trstinst_FI)
-assign("mean_trstinst_FI", 6.051)
-
-trstinst_FR <- ESS.data[c(21023:22990),c(6,13:19)]
-summary(trstinst_FR)
-assign("mean_trstinst_FR", 4.403)
-
-trstinst_GB <- ESS.data[c(22991:25276),c(6,13:19)]
-summary(trstinst_GB)
-assign("mean_trstinst_GB", 4.557)
-
-trstinst_HU <- ESS.data[c(25277:27290),c(6,13:19)]
-summary(trstinst_HU)
-assign("mean_trstinst_HU", 4.214)
-
-trstinst <- ESS.data[c(27291:29918),c(6,13:19)]
-summary(trstinst_IE)
-assign("mean_trstinst_IE", 4.470)
-
-trstinst_IL <- ESS.data[c(29919:32426),c(6,13:19)]
-summary(trstinst_IL)
-assign("mean_trstinst_IL", 4.099)
-
-trstinst_IS <- ESS.data[c(32427:33178),c(6,13:19)]
-summary(trstinst_IS)
-assign("mean_trstinst_IS", 5.126)
-
-trstinst_IT <- ESS.data[c(33179:34138),c(6,13:19)]
-summary(trstinst_IT)
-assign("mean_trstinst_IT", 3.892)
-
-trstinst_LT <- ESS.data[c(34139:36247),c(6,13:19)]
-summary(trstinst_LT)
-assign("mean_trstinst_LT", 4.037)
-
-trstinst_NL <- ESS.data[c(36248:38092),c(6,13:19)]
-summary(trstinst_NL)
-assign("mean_trstinst_NL", 5.443)
-
-trstinst_NO <- ESS.data[c(38093:39716),c(6,13:19)]
-summary(trstinst_NO)
-assign("mean_trstinst_NO", 6.073)
-
-trstinst_PL <- ESS.data[c(39717:41614),c(6,13:19)]
-summary(trstinst_PL)
-assign("mean_trstinst_PL", 3.652)
-
-trstinst_PT <- ESS.data[c(42615:43765),c(6,13:19)]
-summary(trstinst_PT)
-assign("mean_trstinst_PT", 3.217)
-
-trstinst_SE <- ESS.data[c(46250:48096),c(6,13:19)]
-summary(trstinst_SE)
-assign("mean_trstinst_SE", 5.643)
-
-trstinst_SI <- ESS.data[c(38097:49353),c(6,13:19)]
-summary(trstinst_SI)
-assign("mean_trstinst_SI", 4.179)
-
-trstinst_SK <- ESS.data[c(49354:51200),c(6,13:19)]
-summary(trstinst_SK)
-assign("mean_trstinst_SK", 3.424)
-
-# Creating a data frame called: df_trstinst
-
-df_trstinst <- data.frame(mean_trstinst_BE,mean_trstinst_BG, mean_trstinst_CH,  mean_trstinst_CY,  mean_trstinst_CZ,  mean_trstinst_DE,  mean_trstinst_DK,  mean_trstinst_EE,  mean_trstinst_ES,  mean_trstinst_FI,  mean_trstinst_FR,  mean_trstinst_GB,  mean_trstinst_HU,  mean_trstinst_IE,  mean_trstinst_IL,  mean_trstinst_IS,  mean_trstinst_IT,  mean_trstinst_LT,  mean_trstinst_NL,  mean_trstinst_NO,  mean_trstinst_PL,  mean_trstinst_PT,  mean_trstinst_SE,  mean_trstinst_SI,  mean_trstinst_SK)
+# Trust in institution Mean 
+ess6 <- ESS6e02_4_spss_1_
+ESS.subset.1 <- subset(ESS.data, select = c(trstprl, trstlgl , trstplc, trstplt, trstprt, trstep, trstun)
+                       options(max.print=9999999)
+trstinst_means <- print(rowMeans(ESS.subset.1, na.rm = TRUE)) 
+# I dont know how to view them all though
 
 #SGI-Data
 #Sorting of the data has been done on Excel and then converted to csv. 
